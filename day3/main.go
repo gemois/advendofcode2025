b391191b460028fa74502fe22401bf58b33a8b26
gmoi <g.moisidis97@gmail.com>
2025-12-04 01:12:59 +0200

    Lobby part-2

--- day3/main.go
+++ day3/main.go
@@ -12,8 +12,9 @@ func main() {
     defer file.Close()
 		scanner := bufio.NewScanner(file)
 	
-		joltage_sum := 0;
+		joltage_sum := 0
 
+		// part 1
     for scanner.Scan() {
 			line := scanner.Text()
 
@@ -57,4 +58,50 @@ func main() {
 		}
 
 		fmt.Println(joltage_sum)
+
+		// part 2
+
+		joltage_target_length := 12
+		twelve_digit_joltage_sum := 0
+
+		file.Seek(0, 0)
+		scanner = bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			batteries := []int{}
+			for _, char := range line {
+				batteries = append(batteries, int(char - '0'))
+			}
+
+			max_battery_locations := make([]int, 0, joltage_target_length)
+
+			left_bound := 0
+			for len(max_battery_locations) < joltage_target_length  {
+				remaining_to_be_found := joltage_target_length - len(max_battery_locations)
+				last_possible_location := len(batteries) - remaining_to_be_found
+
+				max_battery := batteries[left_bound]
+				max_index := left_bound
+				for i := left_bound; i <= last_possible_location; i++ {
+					if batteries[i] > max_battery {
+						max_battery = batteries[i]
+						max_index = i
+					}
+				}
+
+				max_battery_locations = append(max_battery_locations, max_index)
+				left_bound = max_index + 1
+			}
+
+			twelve_digit_battery_joltage_str := ""
+			for _, index := range max_battery_locations {
+				twelve_digit_battery_joltage_str += strconv.Itoa(batteries[index])
+			}
+
+			twelve_digit_battery_joltage,_ := strconv.Atoi(twelve_digit_battery_joltage_str)
+			twelve_digit_joltage_sum += twelve_digit_battery_joltage
+		}
+
+		fmt.Println(twelve_digit_joltage_sum)
 }
